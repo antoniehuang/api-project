ee882dc3391cec9663a8a72cff360c74b398b402
antoniehuang <antonieyuchi@gmail.com>
2022-08-07 15:46:30 +0100

    working api

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+// Object
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Shéyaa Bin Abraham- Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Johnathan Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'unknown': {
+        'age': 'unknown',
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'
+    }
+
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response) => {
+    const rapperName = request.params.name.toLowerCase()
+    if (rappers[rapperName]) {
+        response.json(rappers[rapperName].birthName)
+    } else {
+        response.json(rappers['unknown'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`This server is now running on port ${PORT}! Better go catch it.`)
+})
